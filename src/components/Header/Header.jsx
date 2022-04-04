d8690b3100a134a1f596bdab536685202adee6ff
Brad Roodt <brad.roodt@gmail.com>
2022-04-04 08:17:20 +0200

    feat: improve nav and autoclose menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,66 +5,102 @@ import FullMenu from '../Menu/FullMenu/FullMenu';
 import SpaIcon from '@mui/icons-material/Spa';
 import { useState } from "react";
 import useMediaQuery from '@mui/material/useMediaQuery';
-import {IconButton, Drawer} from '@mui/material';
+import { IconButton, Drawer } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-// import { makeStyles } from "@material-ui/core/styles";
-// const useStyles = makeStyles({
-//   list: {
-//     width: 250
-//   },
-//   fullList: {
-//     width: "auto"
-//   },
-//   paper: {
-//     background: "blue"
-//   }
-// });
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import Divider from '@mui/material/Divider';
+import MailIcon from '@mui/icons-material/Mail';
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
-  
-  let header;  
+
+  let header;
   // const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:760px)');
-  const [state, setState] = useState({left:false});
+  const navigate = useNavigate();
+  const [state, setState] = useState({ left: false });
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
     }
 
-    setState({ ...state, [anchor]: open });     
+    setState({ ...state, [anchor]: open });
   };
+
+  const onClickNav = (route, event) => {
+    console.log(route.toLowerCase());
+    navigate(`/${route.toLowerCase()}`);
+    setState({ ...state, ['left']: false });
+  }
 
   if (isMobile) {
     header = <>
-    <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={toggleDrawer('left', true)}
-          >
-            <MenuIcon />
-          </IconButton>
-    <Drawer
-      anchor="left"
-      open={state.left}      
-      onClose={toggleDrawer('left', false)}      
-    >
-      <Link to="/about" className="header__full-menu-link">
-        
-          <h4 className="header__title">About</h4>
-        
-      </Link>
-    </Drawer>
-    <div className="header__spacer"></div>
-    <Link to="/" className="header__full-menu-link">
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={toggleDrawer('left', true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        anchor="left"
+        open={state.left}
+        onClose={toggleDrawer('left', false)}
+        sx={{
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 300 },
+        }}
+      >
+
+        <Box
+          sx={{ width: 300 }}
+          role="presentation"
+          onClick={toggleDrawer('left', false)}
+          onKeyDown={toggleDrawer('left', false)}
+        >
+          <nav aria-label="main mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton onClick={event => onClickNav("", event)} >
+                  <ListItemIcon style={{ minWidth: '45px' }}>
+                    <SpaIcon></SpaIcon>
+                  </ListItemIcon>
+                  <h1 className="drawer-list__title" color="primary">PARKS IMPROVEMENT PROJECT</h1>
+                </ListItemButton>
+              </ListItem>
+              <Divider />
+              {['ABOUT', 'PROJECTS', 'EVENTS'].map((text, index) => (
+                <ListItem key={text} disablePadding>
+                  <ListItemButton onClick={event => onClickNav(text, event)}>
+                    <ListItemIcon style={{ minWidth: '45px' }}>
+                      <MailIcon />
+                    </ListItemIcon>
+                    {text}
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </nav>
+        </Box>
+
+      </Drawer>
+      <div className="header__spacer"></div>
+      <Link to="/" className="header__full-menu-link">
         <div className="header__title-wrapper">
           <SpaIcon color="primary"></SpaIcon>
           <h1 className="header__title">PIP</h1>
         </div>
-      </Link>      
+      </Link>
     </>;
   } else {
     header = <>
@@ -81,7 +117,7 @@ function Header() {
   return (
     <>
       <header className="header">
-      {header}
+        {header}
       </header>
     </>
 
